cecbd41e60096c529bc778edcacf3a917f1be62b
e1025735 <e1025735@users.noreply.github.com>
2016-04-06 15:28:35 +0200

    Add more documentation

--- Model/initDisco.R
+++ Model/initDisco.R
@@ -3,6 +3,7 @@
 #Load packages
 #library(TEST);
 
+#Input Parameters from NetLogo?
 #Set the seed to a specific value for reproducibility
 set.seed(123);
 #Anzahl der Männer
@@ -12,21 +13,27 @@ numberOfWomen <- 10;
 
 #Disco Dataframe
 discoDF <- data.frame("id"=character(0), "name"=character(0), "maxMatches"=numeric(0), "side"=numeric(0), "partnerList"=character(0), "rank"=character(0), stringsAsFactors = FALSE);
-
+#Create the differnet genders
 genders <- c("male", "female");
 id = 0;
+#Create a temporary data frame for the individual
 individual <- data.frame("id"=character(1), "name"=character(1), "maxMatches"=numeric(1), "side"=numeric(1), "partnerList"=character(1), "rank"=character(1), stringsAsFactors = FALSE);
 
 #Create for each gender
 for(i in 1:length(genders)){
   #the amount of individuals
   tmpLoopCounter <- ifelse(genders[i]=='female', numberOfMen, numberOfWomen);
+  #set some values for each individual
   for(j in 1:tmpLoopCounter){
-    #set some values for each individual
+    #increment the id by 1
     id <- id + 1;
+    #the the current id to the individual
     individual$id <- id;
+    #add an name to the individual which depends on his gender and the id
     individual$name <- paste(c(genders[i],j), collapse="");
+    #the individual is monogamic
     individual$maxMatches <- 1;
+    #the individual is on the side of his gender
     individual$side <- i;
     #add the individual to the discoDF
     discoDF <- rbind(discoDF, individual);
