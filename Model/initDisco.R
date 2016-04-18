8ec4263dada5cb24011f352a8d34a4cbf058f900
e1025735 <e1025735@users.noreply.github.com>
2016-04-18 15:46:32 +0200

    Update the names of the csv file output to the new docuemntation

--- Model/initDisco.R
+++ Model/initDisco.R
@@ -32,12 +32,12 @@ pickyLower <- ifelse(is.na(args[4]), 0, as.integer(args[4]));
 pickyUpper <- ifelse(is.na(args[5]), 0, as.integer(args[5]));
 
 #Disco Dataframe
-discoDF <- data.frame("id"=character(0), "name"=character(0), "maxMatches"=numeric(0), "side"=numeric(0), "partnerList"=character(0), "rank"=character(0), stringsAsFactors = FALSE);
+discoDF <- data.frame("id"=character(0), "name"=character(0), "maxMatchesInt"=numeric(0), "sideInt"=numeric(0), "partnerList"=character(0), "rankList"=character(0), stringsAsFactors = FALSE);
 #Create the differnet genders
 genders <- c("male", "female");
 id = 0;
 #Create a temporary data frame for the individual
-individual <- data.frame("id"=character(1), "name"=character(1), "maxMatches"=numeric(1), "side"=numeric(1), "partnerList"=character(1), "rank"=character(1), stringsAsFactors = FALSE);
+individual <- data.frame("id"=character(1), "name"=character(1), "maxMatchesInt"=numeric(1), "sideInt"=numeric(1), "partnerList"=character(1), "rankList"=character(1), stringsAsFactors = FALSE);
 
 #Create for each gender
 for(i in 1:length(genders)){
@@ -52,9 +52,9 @@ for(i in 1:length(genders)){
     #add an name to the individual which depends on his gender and the id
     individual$name <- paste(c(genders[i],j), collapse="");
     #the individual is monogamic
-    individual$maxMatches <- 1;
+    individual$maxMatchesInt <- 1;
     #the individual is on the side of his gender
-    individual$side <- i;
+    individual$sideInt <- i;
     #add the individual to the discoDF
     discoDF <- rbind(discoDF, individual);
   }
@@ -85,7 +85,7 @@ for(i in 1:nrow(discoDF)){
   #collapse them into a string
   stringRankList <- paste(rankList, collapse="#");
   #save this into the rank of the individual
-  discoDF$rank[i] <- stringRankList;
+  discoDF$rankList[i] <- stringRankList;
 }
 
 View(discoDF);
